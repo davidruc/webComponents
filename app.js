a7a99abdc890563c963eb819a78ccbfe1b7d0fad
davidruc <davidarueda18@gmail.com>
2023-05-03 09:09:45 -0500

    feat: :tada: Plantilla con webComponents

---
+++ app.js
@@ -0,0 +1,5 @@
+import myHeader from "./components/my-header.js";
+import mySection from "./components/my-section.js";
+import myNav from "./components/my-nav.js";
+import mySelection from "./components/my-selecction.js";
+import myFooter from "./components/my-footer.js";
---
+++ components/my-selecction.js
@@ -0,0 +1,16 @@
+let pathName = new URL(import.meta.url).pathname;
+let name = pathName.split("/").pop().replace(".js", "");
+
+export default class mySelection extends HTMLElement{
+    static async components(){
+        return await ( await fetch(pathName.replace(".js", ".html"))).text();
+    }
+    constructor(){
+        super();
+        this.attachShadow({mode: "open"});
+        Promise.resolve(mySelection.components()).then(html=>{
+            this.shadowRoot.innerHTML = html;
+        })
+    }
+}
+customElements.define(name, mySelection);
